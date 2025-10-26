a3f3ec90c779f2b2dbe974dd41f6fc82f4b18c56
蒼摩 大村 <csso22020@g.nihon-u.ac.jp>
2025-10-26 23:34:13 +0900

    ローカル変更をコミット

--- test.m
+++ test.m
@@ -4,22 +4,33 @@ close all
 bus_x = 184;
 bus_y = 184;
 bus_z = 170;
-base = 83.481;
-theta1 = 1.9*pi/180;
-boom_top = 3900+base;
-boom_clear = 130.108;
-A = boom_top-boom_clear;
-x = A/cos(theta1);
+base = 83.481; %原点からブームの根本までの距離
+theta1 = 1.934*pi/180;
+boom = 3900;
 
-theta2 =2.525*pi/180;
-s  = sqrt(bus_z^2+x^2);
-s1 = x/cos(theta2); % カメラボックスからのブーム先端までの距離
+boom_top = boom+base;
 
-forcus = 3.04;
+x1 = boom*cos(theta1)
+
+x2 = boom*sin(theta1)
+x3 = sqrt((138.21)^2-x2^2);
+
+y = x1-x3;
+
+boom_R = 12.71;
+memb_plate = 70;
+boom_plate = 46.099;
+
+theta2 =2*pi/180;
+high = bus_z+112+33+(memb_plate-boom_plate+boom_R)+boom*sin(theta2); %カメラボックスからブーム前端までの高さ
+s  = sqrt(high^2+y^2);
+s1 = y/cos(theta2); % カメラボックスからのブーム先端までの距離
+
+forcus = 3.04;% 焦点距離
 sensor_size_beside = 3.68;
 sensor_size_vertical = 2.76; 
-renge_beside  = (sensor_size_beside/forcus)*s;
-renge_vertical= (sensor_size_vertical/forcus)*s;
+renge_beside  = (sensor_size_beside/forcus)*s
+renge_vertical= (sensor_size_vertical/forcus)*s
 pixel_beside  = 3280;
 pixel_vertical = 2464;
 
